746c7257a99a954d84cee213ec2d3378c307c1c2
luxradio <112213216+luxradio@users.noreply.github.com>
2023-03-04 14:37:58 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,84 @@
+<script src="./extraluxlux.github.io_files/script.min.js.download"></script>
+<link rel="stylesheet" href="./extraluxlux.github.io_files/style.css">
+<style>  
+#ghost {  
+opacity:0.7;  
+position:absolute;  
+top:0;  
+right:200px;  
+}  
+  
+*{cursor:url(https://u.cubeupload.com/extralux/o7mBPM6.png),auto!important}  
+}  
+  
+*{transition: all ease-in-out 0.2s}  
+  
+body {  
+ overflow-x:hidden;  
+ background:url()6px 80px no-repeat,  
+ url()top center no-repeat,  
+ url(https://i.imgur.com/CTtgxeT.png)top 130px center no-repeat,url(https://u.cubeupload.com/extralux/pbOe7Lk2.jpg)top center repeat-x,url(x)top center/cover fixed}  
+ *{font-family:Helvetica}a{color:#0CF !important}  
+ a:hover{color:#3E020B !important}not{  
+a{a),auto}  
+</style> 
+  
+
+  	
+    
+  
+
+
+ <script>
+
+
+		var message="Isključeno kopiranje i desni klik!";
+
+		function clickIE4(){
+		if (event.button==2){
+		alert(message);
+		return false;
+		}
+		}
+
+		function clickNS4(e){
+		if (document.layers||document.getElementById&&!document.all){
+		if (e.which==2||e.which==3){
+		alert(message);
+		return false;
+		}
+		}
+		}
+
+		if (document.layers){
+		document.captureEvents(Event.MOUSEDOWN);
+		document.onmousedown=clickNS4;
+		}
+		else if (document.all&&!document.getElementById){
+		document.onmousedown=clickIE4;
+		}
+
+		document.oncontextmenu=new Function("alert(message);return false")
+</script>	
+
+
+
+
+
+<script>
+
+
+		function disableselect(e){
+		return false
+		}
+		function reEnable(){
+		return true
+		}
+		document.onselectstart=new Function ("return false")
+		if (window.sidebar){
+		document.onmousedown=disableselect
+		document.onclick=reEnable
+		}
+</script>  
+
+</body></html>
